6578504c82f55a3230f22f93073797ce2a84b642
marcmyworld <moviesminister@gmail.com>
2026-09-17 22:37:57 +0530

    chenfeng: Remove touchscreen reset logic from runatboot.sh
    Remove the legacy Goodix touch reset subroutine from runatboot.sh entirely.

--- recovery/root/system/bin/runatboot.sh
+++ recovery/root/system/bin/runatboot.sh
@@ -7,21 +7,9 @@ LOGMSG() {
 	echo "I:$@" >> /tmp/recovery.log
 }
 
-reset_touch() {
-	if [ -d /sys/devices/platform/goodix_ts.0 ]; then
-		LOGMSG "Resetting Goodix touchscreen post screen blank..."
-		echo 1 > /sys/devices/platform/goodix_ts.0/irq_info
-		echo 1 > /sys/devices/platform/goodix_ts.0/reset
-	else
-		LOGMSG "Goodix touchscreen sysfs not present; skipping reset..."
-	fi
-}
-
 SCRIPT_NAME="$(basename "$0")"
 
 LOGMSG "---$SCRIPT_NAME start---"
-
-reset_touch
 
 /sbin/prune_historic_logs.sh 10
 
